084dc3e3057d33d4fe280ba28cae5cc21bf974aa
smarttommyau <smarttommyau@gmail.com>
2023-07-16 14:25:01 +0800

    Add auto approach to a time stamp use datetime instead of string to process

--- forecast_decode.py
+++ forecast_decode.py
@@ -1,93 +1,127 @@
 import numpy as np
 import netCDF4 as nc
 import os
+from datetime import datetime
 
 # The date and time of the initial field
-date = '2023-07-02'
-time = '23:00' 
+date_time = datetime(
+    year=2023, 
+    month=7, 
+    day=9,
+    hour=23,
+    minute=0)
+date_time_final = datetime(
+    year=2023,
+    month=7,
+    day=17,
+    hour=23,
+    minute=0)
+no_samples = 9
+ 
 
 # The directory for forecasts
 forecast_dir = os.path.join(
-    os.path.join(os.getcwd(), "forecasts"), 
-    ## replace to prevent invaild char ":"
-    date + "-" + time.replace(":", "-")
+    os.path.join(os.getcwd(), "results"),
+    date_time.strftime("%Y-%m-%d-%H-%M") + "to" + date_time_final.strftime("%Y-%m-%d-%H-%M")
 )
-surface_file = os.path.join(forecast_dir, 'output_surface.npy')
-upper_file = os.path.join(forecast_dir, 'output_upper.npy')
+for i in range(no_samples):
+    surface_file = os.path.join(forecast_dir, "output_surface_"+str(i)+".npy")
+    upper_file = os.path.join(forecast_dir, "output_upper_"+str(i)+".npy")
 
 # Load the saved numpy arrays
-surface_data = np.load(surface_file)
-mean_sea_level_pressure = surface_data[0]
-u_component_of_wind_10m = surface_data[1]
-v_component_of_wind_10m = surface_data[2]
-temperature_2m = surface_data[3]
+    surface_data = np.load(surface_file)
+    mean_sea_level_pressure = surface_data[0]
+    u_component_of_wind_10m = surface_data[1]
+    v_component_of_wind_10m = surface_data[2]
+    temperature_2m = surface_data[3]
 
-upper_data = np.load(upper_file)
-geopotential = upper_data[0]
-specific_humidity = upper_data[1]
-temperature = upper_data[2]
-u_component_of_wind = upper_data[3]
-v_component_of_wind = upper_data[4]
+    upper_data = np.load(upper_file)
+    geopotential = upper_data[0]
+    specific_humidity = upper_data[1]
+    temperature = upper_data[2]
+    u_component_of_wind = upper_data[3]
+    v_component_of_wind = upper_data[4]
 
-with nc.Dataset(os.path.join(forecast_dir, 'output_surface.nc'), 'w', format='NETCDF4_CLASSIC') as nc_file:
-    # Create dimensions
-    nc_file.createDimension('longitude', 1440)
-    nc_file.createDimension('latitude', 721)
+    with nc.Dataset(
+        os.path.join(forecast_dir, "output_surface_"+str(i)+".nc"), "w", format="NETCDF4_CLASSIC"
+    ) as nc_file:
+        # Create dimensions
+        nc_file.createDimension("longitude", 1440)
+        nc_file.createDimension("latitude", 721)
 
-    # Create variables
-    nc_lon = nc_file.createVariable('longitude', np.float32, ('longitude',))
-    nc_lat = nc_file.createVariable('latitude', np.float32, ('latitude',))
-    nc_msl = nc_file.createVariable('mean_sea_level_pressure', np.float32, ('latitude', 'longitude'))
-    nc_u10 = nc_file.createVariable('u_component_of_wind_10m', np.float32, ('latitude', 'longitude'))
-    nc_v10 = nc_file.createVariable('v_component_of_wind_10m', np.float32, ('latitude', 'longitude'))
-    nc_t2m = nc_file.createVariable('temperature_2m', np.float32, ('latitude', 'longitude'))
+        # Create variables
+        nc_lon = nc_file.createVariable("longitude", np.float32, ("longitude",))
+        nc_lat = nc_file.createVariable("latitude", np.float32, ("latitude",))
+        nc_msl = nc_file.createVariable(
+            "mean_sea_level_pressure", np.float32, ("latitude", "longitude")
+        )
+        nc_u10 = nc_file.createVariable(
+            "u_component_of_wind_10m", np.float32, ("latitude", "longitude")
+        )
+        nc_v10 = nc_file.createVariable(
+            "v_component_of_wind_10m", np.float32, ("latitude", "longitude")
+        )
+        nc_t2m = nc_file.createVariable(
+            "temperature_2m", np.float32, ("latitude", "longitude")
+        )
 
-    # Set variable attributes
-    nc_lon.units = 'degrees_east'
-    nc_lat.units = 'degrees_north'
-    nc_msl.units = 'Pa'
-    nc_u10.units = 'm/s'
-    nc_v10.units = 'm/s'
-    nc_t2m.units = 'K'
+        # Set variable attributes
+        nc_lon.units = "degrees_east"
+        nc_lat.units = "degrees_north"
+        nc_msl.units = "Pa"
+        nc_u10.units = "m/s"
+        nc_v10.units = "m/s"
+        nc_t2m.units = "K"
 
-    # Write data to variables
-    nc_lon[:] = np.linspace(0.125, 359.875, 1440)
-    nc_lat[:] = np.linspace(90, -90, 721)
-    nc_msl[:] = mean_sea_level_pressure
-    nc_u10[:] = u_component_of_wind_10m
-    nc_v10[:] = v_component_of_wind_10m
-    nc_t2m[:] = temperature_2m
+        # Write data to variables
+        nc_lon[:] = np.linspace(0.125, 359.875, 1440)
+        nc_lat[:] = np.linspace(90, -90, 721)
+        nc_msl[:] = mean_sea_level_pressure
+        nc_u10[:] = u_component_of_wind_10m
+        nc_v10[:] = v_component_of_wind_10m
+        nc_t2m[:] = temperature_2m
 
-with nc.Dataset(os.path.join(forecast_dir, 'output_upper.nc'), 'w', format='NETCDF4_CLASSIC') as nc_file:
-    # Create dimensions
-    nc_file.createDimension('longitude', 1440)
-    nc_file.createDimension('latitude', 721)
-    nc_file.createDimension('level', 13)
+    with nc.Dataset(
+        os.path.join(forecast_dir, "output_upper_"+str(i)+".nc"), "w", format="NETCDF4_CLASSIC"
+    ) as nc_file:
+        # Create dimensions
+        nc_file.createDimension("longitude", 1440)
+        nc_file.createDimension("latitude", 721)
+        nc_file.createDimension("level", 13)
 
-    # Create variables
-    nc_lon = nc_file.createVariable('longitude', np.float32, ('longitude',))
-    nc_lat = nc_file.createVariable('latitude', np.float32, ('latitude',))
-    nc_geopotential = nc_file.createVariable('geopotential', np.float32, ('level', 'latitude', 'longitude'))
-    nc_specific_humidity = nc_file.createVariable('specific_humidity', np.float32, ('level', 'latitude', 'longitude'))
-    nc_temperature = nc_file.createVariable('temperature', np.float32, ('level', 'latitude', 'longitude'))
-    nc_u_component_of_wind = nc_file.createVariable('u_component_of_wind', np.float32, ('level', 'latitude', 'longitude'))
-    nc_v_component_of_wind = nc_file.createVariable('v_component_of_wind', np.float32, ('level', 'latitude', 'longitude'))
-    
+        # Create variables
+        nc_lon = nc_file.createVariable("longitude", np.float32, ("longitude",))
+        nc_lat = nc_file.createVariable("latitude", np.float32, ("latitude",))
+        nc_geopotential = nc_file.createVariable(
+            "geopotential", np.float32, ("level", "latitude", "longitude")
+        )
+        nc_specific_humidity = nc_file.createVariable(
+            "specific_humidity", np.float32, ("level", "latitude", "longitude")
+        )
+        nc_temperature = nc_file.createVariable(
+            "temperature", np.float32, ("level", "latitude", "longitude")
+        )
+        nc_u_component_of_wind = nc_file.createVariable(
+            "u_component_of_wind", np.float32, ("level", "latitude", "longitude")
+        )
+        nc_v_component_of_wind = nc_file.createVariable(
+            "v_component_of_wind", np.float32, ("level", "latitude", "longitude")
+        )
 
-    # Set variable attributes
-    nc_lon.units = 'degrees_east'
-    nc_lat.units = 'degrees_north'
-    nc_geopotential.units = 'm'
-    nc_specific_humidity.units = 'kg/kg'
-    nc_temperature.units = 'K'
-    nc_u_component_of_wind.units = 'm/s'
-    nc_v_component_of_wind.units = 'm/s'
+        # Set variable attributes
+        nc_lon.units = "degrees_east"
+        nc_lat.units = "degrees_north"
+        nc_geopotential.units = "m"
+        nc_specific_humidity.units = "kg/kg"
+        nc_temperature.units = "K"
+        nc_u_component_of_wind.units = "m/s"
+        nc_v_component_of_wind.units = "m/s"
 
-    # Write data to variables
-    nc_lon[:] = np.linspace(0.125, 359.875, 1440)
-    nc_lat[:] = np.linspace(90, -90, 721)
-    nc_geopotential[:] = geopotential
-    nc_specific_humidity[:] = specific_humidity
-    nc_temperature[:] = temperature
-    nc_u_component_of_wind[:] = u_component_of_wind
-    nc_v_component_of_wind[:] = v_component_of_wind
+        # Write data to variables
+        nc_lon[:] = np.linspace(0.125, 359.875, 1440)
+        nc_lat[:] = np.linspace(90, -90, 721)
+        nc_geopotential[:] = geopotential
+        nc_specific_humidity[:] = specific_humidity
+        nc_temperature[:] = temperature
+        nc_u_component_of_wind[:] = u_component_of_wind
+        nc_v_component_of_wind[:] = v_component_of_wind
--- inference.py
+++ inference.py
@@ -2,54 +2,128 @@ import os
 import numpy as np
 import onnx
 import onnxruntime as ort
-
+from datetime import datetime, timedelta
 # Use GPU or CPU
 use_GPU = False
 
 # The date and time of the initial field
-date = '2023-07-02'
-time = '23:00'
+# date = '2023-07-03'
+date_time = datetime(
+    year=2023, 
+    month=7, 
+    day=9,
+    hour=23,
+    minute=0)
+# time = '23:00'
 
-# Uncomment the model to be used
-model_used = 'models/pangu_weather_24.onnx' # 24h
-# model_used = 'models/pangu_weather_6.onnx' # 6h
-# model_used = 'models/pangu_weather_3.onnx' # 3h
-# model_used = 'models/pangu_weather_1.onnx' # 1h
+# The date and time of the final approaches
+date_time_final = datetime(
+    year=2023, 
+    month=7, 
+    day=17,
+    hour=23,
+    minute=0)
+
+final_result_dir = os.path.join(
+    os.path.join(os.getcwd(), "results"),
+    (date_time.strftime("%Y-%m-%d-%H-%M") + "to" + date_time_final.strftime("%Y-%m-%d-%H-%M"))
+)
+os.makedirs(final_result_dir,exist_ok=True)
+
+temp_dir = os.path.join(os.getcwd(), "temp")
+os.makedirs(temp_dir,exist_ok=True)
+## copy forecast files to temp dir
+
+model_24 = 'models/pangu_weather_24.onnx' # 24h
+model_6 = 'models/pangu_weather_6.onnx' # 6h
+model_3 = 'models/pangu_weather_3.onnx' # 3h
+model_1 = 'models/pangu_weather_1.onnx' # 1h
+
+
 
 # The directory for forecasts
 forecast_dir = os.path.join(
     os.path.join(os.getcwd(), "forecasts"),
     ## replace to prevent invaild char ":"
-    date + "-" + time.replace(":", "-"),
+    date_time.strftime("%Y-%m-%d-%H-%M"),
 )
+# Calculate the order of models should be used to generate the final result
+time_difference_in_hour = (date_time_final - date_time).total_seconds() / 3600
+current_date_time = date_time
+last_date_time = None
+model_used = None
+start = True
+i = 0
+ort_session = None
+jump = False
+while time_difference_in_hour >= 1:
+    print(time_difference_in_hour)
+    print(i)
+    last_model = model_used
+    if time_difference_in_hour >= 24:
+        model_used = model_24
+        time_difference_in_hour -= 24
+        current_date_time += timedelta(hours=24)
+        print("24")
+    elif time_difference_in_hour >= 6:
+        model_used = model_6
+        time_difference_in_hour -= 6
+        current_date_time += timedelta(hours=6)
+        print("6")
+    elif time_difference_in_hour >= 3:
+        model_used = model_3
+        time_difference_in_hour -= 3
+        current_date_time += timedelta(hours=3)
+        print("3")
+    elif time_difference_in_hour >= 1:
+        model_used = model_1
+        time_difference_in_hour -= 1
+        current_date_time += timedelta(hours=1)
+        print("1")
+    if model_used == last_model:
+        jump = True
+    else:
+        jump = False
+    print(current_date_time.strftime("%Y-%m-%d-%H-%M"))
 
-# Load the model
-model = onnx.load(model_used)
+    if not jump:
+        # Load the model
+        model = onnx.load(model_used)
 
-# Set the behavier of onnxruntime
-options = ort.SessionOptions()
-options.enable_cpu_mem_arena=False
-options.enable_mem_pattern = False
-options.enable_mem_reuse = False
-# Increase the number for faster inference and more memory consumption
-options.intra_op_num_threads = 30
+        # Set the behavier of onnxruntime
+        options = ort.SessionOptions()
+        options.enable_cpu_mem_arena=False
+        options.enable_mem_pattern = False
+        options.enable_mem_reuse = False
+        # Increase the number for faster inference and more memory consumption
+        options.intra_op_num_threads = 30
 
-# Set the behavier of cuda provider
-cuda_provider_options = {'arena_extend_strategy':'kSameAsRequested',}
+        # Set the behavier of cuda provider
+        cuda_provider_options = {'arena_extend_strategy':'kSameAsRequested',}
 
-# Initialize onnxruntime session for Pangu-Weather Models
-if use_GPU:
-    ort_session = ort.InferenceSession(model_used, sess_options=options, providers=[('CUDAExecutionProvider', cuda_provider_options)])
-else:
-    ort_session = ort.InferenceSession(model_used, sess_options=options, providers=['CPUExecutionProvider'])
+        # Initialize onnxruntime session for Pangu-Weather Models
+        if use_GPU:
+            ort_session = ort.InferenceSession(model_used, sess_options=options, providers=[('CUDAExecutionProvider', cuda_provider_options)])
+        else:
+            ort_session = ort.InferenceSession(model_used, sess_options=options, providers=['CPUExecutionProvider'])
 
-# Load the upper-air numpy arrays
-input = np.load(os.path.join(forecast_dir, 'input_upper.npy')).astype(np.float32)
-# Load the surface numpy arrays
-input_surface = np.load(os.path.join(forecast_dir, 'input_surface.npy')).astype(np.float32)
+    print("start")
+    # Load the upper-air numpy arrays
+    # Load the surface numpy arrays
+    input = None
+    input_surface = None
+    if start:
+        input = np.load(os.path.join(forecast_dir, 'input_upper.npy')).astype(np.float32)
+        input_surface = np.load(os.path.join(forecast_dir, 'input_surface.npy')).astype(np.float32)
+    else:
+        input = np.load(os.path.join(final_result_dir, 'output_upper_'+last_date_time.strftime("%Y-%m-%d-%H-%M")+'.npy')).astype(np.float32)
+        input_surface = np.load(os.path.join(final_result_dir, 'output_surface_'+last_date_time.strftime("%Y-%m-%d-%H-%M")+'.npy')).astype(np.float32)
 
-# Run the inference session
-output, output_surface = ort_session.run(None, {'input':input, 'input_surface':input_surface})
-# Save the results
-np.save(os.path.join(forecast_dir, 'output_upper'), output)
-np.save(os.path.join(forecast_dir, 'output_surface'), output_surface)
+    # Run the inference session
+    output, output_surface = ort_session.run(None, {'input':input, 'input_surface':input_surface})
+    # Save the results
+    np.save(os.path.join(final_result_dir, 'output_upper_'+current_date_time.strftime("%Y-%m-%d-%H-%M")), output)
+    np.save(os.path.join(final_result_dir, 'output_surface_' + current_date_time.strftime("%Y-%m-%d-%H-%M")), output_surface)
+    last_date_time = current_date_time
+    start = False
+    i += 1
